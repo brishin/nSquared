a1e0e059ed00ccf894bdf3afb3acf88c97959933
Brian Donghee Shin <shindonghee@gmail.com>
2012-06-18 16:26:45 -0400

    Fix JSON

--- scripts/xml_parser.py
+++ scripts/xml_parser.py
@@ -8,33 +8,42 @@ else:
   file_path = '/Users/brian/Downloads/download.rss'
 
 input_file = open(file_path)
-soup = BeautifulSoup(input_file)
+# Most lenient parser
+soup = BeautifulSoup(input_file, "html.parser")
 
 items = []
 for item in soup.find_all('item'):
-  temp = {} 
+  temp = {}
+  temp_aggregate = {}
   for child in item.children:
     try:
-      if child.name == 'link' and child.text == '':
-        temp['link'] = str(child.next_element).strip()
+      if child.name in temp:
+        if child.name not in temp_aggregate:
+          temp_aggregate[child.name] = [] 
+        if temp[child.name] is not None:
+          temp_aggregate[child.name].append(temp[child.name])
+            
+        temp[child.name] = None
+        temp_aggregate[child.name].append(child.text)
       else:
         temp[child.name] = child.text
     except AttributeError, e:
       pass
 
+  for tag in temp_aggregate:
+      temp[tag] = temp_aggregate[tag] 
+
   # Finding a picture
   try:
-    pics = item.description.find_all('img') or []
+    pics = BeautifulSoup(item.description.text).find_all('img') or []
     if (pics):
       biggest_img = max(pics, key=lambda pic: pic.get('height'))
       temp['img'] = biggest_img.get('data-lazy-src') or \
           biggest_img.get('src')
   except AttributeError, e:
     pass
-
+  print len(temp)
   items.append(temp)
-
-    
 
 output_file = open(file_path + '.json', 'w')
 print("Outputting to" + file_path)
