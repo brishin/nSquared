136410427fb4b47db73c3b76f0cd86aae05972ff
Brian Donghee Shin <shindonghee@gmail.com>
2012-06-28 17:51:00 -0400

    Experiments with Rainbows

--- rainbowss/RImage.py
+++ rainbowss/RImage.py
@@ -1,8 +1,17 @@
-import Image from PIL
-import numpy, colorsys
+from PIL import Image
+import numpy as np
+import colorsys
 
 class RImage:
   def __init__(self, path):
     image = Image.open(path)
-    self.image_array = numpy.asarray(image)
-    colorsys.rgb_to_hsv(*s[0,0])
+    self.image_array = np.asarray(image)
+    for cell in for x in self.image_array:
+
+    for cell in (cell for row in a for cell in row):
+      cell = colorsys.rgb_to_hsv(*cell)
+
+
+im = Image.open('IMG_20110603_162102.jpg')
+a = np.asarray(im)
+b = a.reshape((-1, 3))
