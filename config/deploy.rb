bef6a0a6b881224fc6074f941b37aa2740600246
Brian Donghee Shin <shindonghee@gmail.com>
2012-07-05 12:57:45 -0400

    Deploy details

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,17 +9,8 @@ set :branch, "master"
 set :deploy_via, :remote_cache
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 
-role :app, "ty"                          # This may be the same as your `Web` server
-set :deploy_to, "/home/ubuntu/www"
+server "ty", :app, :web, :db, :primary => true
+set :deploy_to, "/home/ubuntu/www/nSquared"
 
 # if you want to clean up old releases on each deploy uncomment this:
-# after "deploy:restart", "deploy:cleanup"
-
-# If you are using Passenger mod_rails uncomment this:
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
+after "deploy:restart", "deploy:cleanup"
