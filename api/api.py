736cd61e1bf0675358ab59837e2a2a624cd294b2
Brian Donghee Shin <shindonghee@gmail.com>
2012-07-09 13:44:10 -0400

    Search using API

--- api/api.py
+++ api/api.py
@@ -6,6 +6,7 @@ app = Flask(__name__)
 app.config['solr_url'] = 'http://localhost:2000/solr/select'
 # Whitelist of parameters allowed to send to solr
 app.config['allowed_params'] = ['rows', 'start']
+app.config['thumbnail_api'] = 'http://209.17.190.27/rcw_wp/0.51.0/cache_image_lookup.php'
 
 def jsonp(func):
     """Wraps JSONified output for JSONP requests."""
@@ -28,14 +29,50 @@ def posts_api():
   if 'domain' in request.args:
     params = build_params(request.args)
     solr_request = requests.get(app.config['solr_url'], params=params)
+    app.logger.debug('GET' + solr_request.url)
     data = json.loads(solr_request.content)
     if 'response' in data and 'docs' in data['response']:
-      return json.dumps(data['response']['docs'])
+      results = data['response']['docs']
+      fetch_thumbnails(request, results)
+      return json.dumps(results)
 
   if solr_request is not None:
     abort(solr_request.status_code)
   else:
     abort(404)
+
+@app.route('/v1/search', methods=['GET'])
+@jsonp
+def search_api():
+  solr_request = None
+  if 'domain' in request.args and 'search' in request.args:
+    params = build_params(request.args)
+    # Solr query
+    params['q'] = params['q'] + '\n' + request.args['search']
+    solr_request = requests.get(app.config['solr_url'], params=params)
+    app.logger.debug('GET(search) ' + solr_request.url)
+
+    data = json.loads(solr_request.content)
+    if 'response' in data and 'docs' in data['response']:
+      results = data['response']['docs']
+      fetch_thumbnails(request, results)
+      return json.dumps(results)
+
+  if solr_request is not None:
+    abort(solr_request.status_code)
+  else:
+    abort(404)
+
+def fetch_thumbnails(request, results):
+  for result in results:
+    if 'media' not in result:
+      break
+    params = {}
+    params['image_url'] = result['media'][0]
+    params['domain'] = request.args['domain']
+    thumbnail = requests.get(app.config['thumbnail_api'], params=params)
+    if thumbnail.status_code == 200:
+      result['thumbnail'] = thumbnail.content
 
 def build_params(args):
   params = {}
