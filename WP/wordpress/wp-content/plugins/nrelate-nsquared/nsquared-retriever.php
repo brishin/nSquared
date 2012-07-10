d448f50fbd042a23d4eb58e35645b50370e23d1f
Jane Cakemaster <janecakemaster@gmail.com>
2012-07-10 10:22:08 -0400

    retriever mod, need to add script path

--- WP/wordpress/wp-content/plugins/nrelate-nsquared/nsquared-retriever.php
+++ WP/wordpress/wp-content/plugins/nrelate-nsquared/nsquared-retriever.php
@@ -1,27 +1,26 @@
 <?php
 
-$cat_json = $tag_json = '';
+$nsq_json = $cat_json = $tag_json = '';
 
-function nrelate_get_cats(){
+function nrelate_get_cats_tags(){
   $cat_args=array(
     'orderby' => 'name',
     'order' => 'ASC');
   $categories=get_categories($cat_args);
   $cat_json = json_encode($categories);
 
-}
-
-function nrelate_get_tags(){
   $tag_args=array(
       'orderby' => 'name',
       'order' => 'ASC');
   $tags=get_tags($tag_args);
   $tag_json = json_encode($tags);
+
+  $cats_tags = array('nsquared_categories' => $cat_json, 'nsquared_tags' => $tag_json);
+  $nsq_json = json_encode($cats_tags);
 }
 
-add_action('init', 'nrelate_get_cats');
-add_action('init', 'nrelate_get_tags');
+add_action('init', 'nrelate_get_cats_tags');
 
-//wp enqueue
-
+wp_enqueue_script( 'nsquared_retriever' );
+wp_localize_script( 'nsquared_retriever', 'NsquaredRetriever', $nsq_json );
 ?>
