a3adf625e284afa5360eae3ff4b1e848fea7929d
Brian Donghee Shin <shindonghee@gmail.com>
2012-07-12 15:57:59 -0400

    Fix api

--- api/api.py
+++ api/api.py
@@ -1,10 +1,10 @@
 from flask import Flask, request, abort, current_app
 from functools import wraps
 import json, requests
-from helpers import find_thumb
+from rainbowss.helpers import find_thumb
 
 app = Flask(__name__)
-app.config['solr_url'] = 'http://10.10.10.31:8443/solr/select'
+app.config['solr_url'] = 'http://localhost:2000/solr/select'
 # Whitelist of parameters allowed to send to solr
 app.config['allowed_params'] = ['rows', 'start']
 app.config['THUMB_URL'] = 'http://209.17.190.27/rcw_wp/0.51.0/cache_image_lookup.php'
@@ -30,7 +30,6 @@ def posts_api():
   if 'domain' in request.args:
     params = build_params(request.args)
     solr_request = requests.get(app.config['solr_url'], params=params)
-    app.logger.debug('GET' + solr_request.url)
     data = json.loads(solr_request.content)
     if 'response' in data and 'docs' in data['response']:
       results = data['response']['docs']
@@ -69,6 +68,7 @@ def fetch_thumb_requests(request, results):
     if 'media' not in result:
       app.logger.debug('media not in result')
       break
+    app.logger.debug(find_thumb(result['media'], request.args['domain']))
     result['thumb_request'] = find_thumb(result['media'], request.args['domain'])
 
 def build_params(args):
