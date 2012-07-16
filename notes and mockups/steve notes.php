edcf564a29e9d0ea906a61a8751e4c7eba0bda54
Jane Cakemaster <janecakemaster@gmail.com>
2012-07-16 16:44:13 -0400

    records including mockups and notes

---
+++ notes and mockups/steve notes.php
@@ -0,0 +1,39 @@
+-related-plugin-status.php
+no backwards compatibility stuff
+
+nrelate_ functions in admin folder
+
+?
+
+
+wp_enqueue_scripts - w/o, would use jquery 3 times
+
+nrelate_related_is_loading()
+not gonna use this
+not using injecting
+
+
+shortcode (WP codex)
+[function in here implemeneted]
+fuction is down there
+
+related_settings folder
+-related-menu.php
+settings pages
+
+nrealte-related-settings.php
+register()
+add_settings_section()
+
+dopage() triggered in related-menu.php
+nrelate_related_options[] all the lists
+wp_remote_post()
+
+
+
+rcw -> nsq
+rc -> sq
+
+
+
+GET CATEGORIES
