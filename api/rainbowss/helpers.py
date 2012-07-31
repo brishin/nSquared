61a43fe6c507dfc222cc9b10f7fbe63280806475
Brian Shin <ds2791@columbia.edu>
2012-07-31 17:27:24 -0400

    Helpers update to SQL

--- api/rainbowss/helpers.py
+++ api/rainbowss/helpers.py
@@ -1,9 +1,11 @@
 import requests
 import sunburnt
+import MySQLdb
 
 THUMB_URL = 'http://209.17.190.27/rcw_wp/0.51.0/cache_image_lookup.php'
 SOLR_URL = 'http://10.10.10.31:8443/solr/'
 solr = sunburnt.SolrInterface(SOLR_URL)
+MYSQL_SETTINGS = '10.10.10.17', 'vulcan', '', 'linksDBProd'
 
 def find_thumb(urls, domain):
   params = {}
@@ -16,23 +18,45 @@ def find_thumb(urls, domain):
   return None
 
 def get_domain(rssid):
-  response = solr.filter(rssid=rssid).paginate(rows=1).execute()
-  if response.status is not 0:
-    return None
-  docs = list(response)
-  if len(docs) > 0:
-    return docs[0].get('domain', None)
+  connection = None
+  domain = None
+  try:
+    (connection, cursor) = connect_mysql()
+    cursor.execute("SELECT keyCode FROM `domains` WHERE rssid = %s", rssid)
+    domain = cursor.fetchone()
+    if domain and len(domain) > 0:
+      domain = domain[0]
+  except Exception, e:
+    raise e
+  finally:
+    if connection:
+      connection.close()
+  return domain
 
 def get_rssid(domain):
-  response = solr.filter(domain=domain).paginate(rows=1).execute()
-  if response.status is not 0:
-    return None
-  docs = list(response)
-  if len(docs) > 0:
-    return docs[0].get('rssid', None)
+  connection = None
+  rssid = None
+  try:
+    (connection, cursor) = connect_mysql()
+    cursor.execute("SELECT rssid FROM `domains` WHERE keyCode = %s", domain)
+    rssid = cursor.fetchone()
+    if rssid and len(rssid) > 0:
+      rssid = rssid[0]
+  except Exception, e:
+    raise e
+  finally:
+    if connection:
+      connection.close()
+  return rssid
+    
 
 def get_num_thumbs(rssid):
-  response = solr.filter(rssid=rssid).paginate(rows=1).execute()
+  response = solr.query().filter(rssid=rssid).paginate(rows=1).execute()
   if response.status is not 0:
     return 0
   return response.result.numFound
+
+def connect_mysql():
+  connection = MySQLdb.connect(*MYSQL_SETTINGS)
+  cursor = connection.cursor()
+  return connection, cursor
