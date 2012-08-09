01c9faee653b68f397dde3501f6971ae56d99683
Brian Shin <ds2791@columbia.edu>
2012-08-09 16:26:51 -0400

    worker fix

--- api/rainbowss/worker.py
+++ api/rainbowss/worker.py
@@ -5,11 +5,8 @@ r = redis.StrictRedis(host='localhost', port=6379, db=0)
 
 def worker_daemon():
   while True:
-    rssid = r.blpop('colorQueue', 0)
-    try:
-      fetcher.insert_thumbs(rssid)
-    except Exception, e:
-      print e
+    (collection, rssid) = r.blpop('colorQueue', 0)
+    fetcher.insert_thumbs(rssid)
 
 if __name__ == '__main__':
   worker_daemon()
