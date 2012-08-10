5f6d8501f366a10109f64ee139868fd7126a843b
Brian Shin <ds2791@columbia.edu>
2012-08-10 19:17:19 -0400

    NO DAEMON in worker

--- api/rainbowss/worker.py
+++ api/rainbowss/worker.py
@@ -1,26 +1,12 @@
 import redis
 import fetcher
-import daemon
-import argparse
-import lockfile
 
 r = redis.StrictRedis(host='localhost', port=6379, db=0)
-
-parser = argparse.ArgumentParser('Color queue worker.')
-parser.add_argument('--pidfile')
-args = vars(parser.parse_args())
-
-if args.get('pidfile'):
-  context = daemon.DaemonContext(
-    pidfile=lockfile.FileLock(args.get('pidfile'))
-    )
-else:
-  context = daemon.DaemonContext()
 
 def worker_daemon():
   while True:
     (collection, rssid) = r.blpop('colorQueue', 0)
     fetcher.insert_thumbs(rssid)
 
-with context:
+if __name__ == '__main__':
   worker_daemon()
