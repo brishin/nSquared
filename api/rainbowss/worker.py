57bc498fe18907186f3b6618800274f0f99c8b23
Brian Shin <ds2791@columbia.edu>
2012-08-10 19:09:39 -0400

    add lockfile

--- api/rainbowss/worker.py
+++ api/rainbowss/worker.py
@@ -2,6 +2,7 @@ import redis
 import fetcher
 import daemon
 import argparse
+import lockfile
 
 r = redis.StrictRedis(host='localhost', port=6379, db=0)
 
@@ -13,11 +14,13 @@ if args.get('pidfile'):
   context = daemon.DaemonContext(
     pidfile=lockfile.FileLock(args.get('pidfile'))
     )
+else:
+  context = daemon.DaemonContext()
 
 def worker_daemon():
   while True:
     (collection, rssid) = r.blpop('colorQueue', 0)
     fetcher.insert_thumbs(rssid)
 
-with daemon.DaemonContext():
+with context:
   worker_daemon()
